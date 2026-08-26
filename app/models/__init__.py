e01cfc3a041ecf814f17fc9c2fcd5fe42bc8c149
R1Jedi <stas.lukin.2014@mail.ru>
2026-08-26 15:42:52 +0300

    Добавление модели корзины и привязки к моделям юзера и продукт

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from .categories import Category
 from .products import Product
 from .users import User, UserRole
 from .reviews import Review
+from .cart_items import CartItem
 
-__all__ = ["Category", "Product", "User", "UserRole", "Review"]
+__all__ = ["Category", "Product", "User", "UserRole", "Review", "CartItem"]
