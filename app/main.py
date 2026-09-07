a706a128a516be23557db61c6c61094f3ec82247
R1Jedi <stas.lukin.2014@mail.ru>
2026-09-07 15:18:54 +0300

    Добавлено логирование

--- app/main.py
+++ app/main.py
@@ -1,18 +1,40 @@
-from fastapi import FastAPI
+from uuid import uuid4
+
+from fastapi import FastAPI, Request
+from loguru import logger
 
 from app.routers import categories, products, users, reviews, cart, orders, payments
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 
 app = FastAPI(
     title="FastAPI Интернет-магазин",
     version='0.5.0'
 )
 
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue=True)
+
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
+
 # app_v1 = FastAPI(
 #     title="FastAPI Интернет-магазин — API v1",
 #     version="0.5.0"
 # )
-#
+
 app.include_router(categories.router)
 app.include_router(products.router)
 app.include_router(users.router)
@@ -20,7 +42,7 @@ app.include_router(reviews.router)
 app.include_router(cart.router)
 app.include_router(orders.router)
 app.include_router(payments.router)
-#
+
 # app.mount("/v1", app_v1)
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
